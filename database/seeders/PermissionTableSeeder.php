b5eaea61192cab6dbcf09750d936a824e8419f24
Samar Alali <alalisummer@gmail.com>
2022-08-15 16:25:54 +0300

    new fiiles

---
+++ database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $permissions = [
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete',
+            'role-view',
+            'role-add',
+            'role-archive'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+}
